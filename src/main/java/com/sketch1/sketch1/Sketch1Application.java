831020e8860e562f1bb92136dd2aef76151bedab
azobs <ckiadjeu@gmail.com>
2023-07-22 02:18:00 +0100

    first commit after project creation and pom configuration

---
+++ src/main/java/com/sketch1/sketch1/Sketch1Application.java
@@ -0,0 +1,13 @@
+package com.sketch1.sketch1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sketch1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sketch1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sketch1/sketch1/Sketch1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sketch1.sketch1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sketch1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
